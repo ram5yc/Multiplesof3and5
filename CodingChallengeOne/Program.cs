e714af67094daf1293a54401fef87bf21dc1897f
ram5yc <ram5yc@mail.missouri.edu>
2021-03-08 18:24:38 -0600

    commit

--- CodingChallengeOne/Program.cs
+++ CodingChallengeOne/Program.cs
@@ -10,6 +10,15 @@ namespace CodingChallengeOne
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 )
+                {
+                    count += i;
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
